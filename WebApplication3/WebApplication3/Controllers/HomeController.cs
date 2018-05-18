a131d41570f58cb206c693b3468f1d8862b5e8d8
Long Thạch Đức <long8a2hgybvn@gmail.com>
2018-05-18 12:32:51 +0700

    Change

---
+++ WebApplication3/WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Search(SearchInput inputstring)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View("Index",inputstring);
+        }
+    }
+}
---
+++ WebApplication3/WebApplication3/Models/SearchInput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication3.Models
+{
+    public class SearchInput
+    {
+        [Required]
+        public string inputstring { get; set; }
+    }
+}
